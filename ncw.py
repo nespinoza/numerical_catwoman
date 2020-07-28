ef1089f056967faf023416d287478d3330cf068b
nespinoza <nespinoza@stsci.edu>
2020-07-28 11:26:08 -0400

    ncw has limb-darkening

--- ncw.py
+++ ncw.py
@@ -51,7 +51,7 @@ def init_catwoman(t, ld_law, nresampling = None, etresampling = None):
          m = catwoman.TransitModel(params, t, supersample_factor=nresampling, exp_time=etresampling)
      return params,m
 
-def numerical_batman(X,Y,rp,npixels = 1000):
+def numerical_batman(X,Y,rp,u,npixels = 1000):
     """
     This function receives X and Y coordinates (in stellar units, assuming zero is at the center of the star) of the 
     position of a planet, and calculates a numerical lightcurve at each of those positions assuming a star of radius 1 
@@ -59,18 +59,39 @@ def numerical_batman(X,Y,rp,npixels = 1000):
     contains both the planet circle and the star circle, that pixel is painted (with a zero). Then we sum all the pixels. 
 
     npixels is always even (for easier modeling). If odd number is given, add 1.
+
+    Parameters
+    ----------
+
+        X: Array of X distance from the host star in stellar radii
+
+        X: Array of Y distance from the host star in stellar radii
+
+        rp: planet-to-star radius ratio
+
+        u: limb-darkening coefficients (quadratic law)
+
+        npixels: number of pixels in X and Y direction to model the star on
+
     """
     # Set npixels to be even:
     if npixels % 2 != 0:
         npixels = npixels + 1
 
+    # Initialize array and star radii:
     star = np.ones([npixels, npixels])
-    # Paint points outside the star with zeroes:
     rpixel = npixels/2
+    # Paint limb-darekning and points outside the star with zeroes:
     for i in range(star.shape[0]):
         for j in range(star.shape[1]):
-            if (i-rpixel)**2 + (j-rpixel)**2 > rpixel**2:
+            # Get distance of current point:
+            d = np.sqrt((i-rpixel)**2 + (j-rpixel)**2)
+            # If distance outside rpixel, set to zero. If not, paint limb-darkening:
+            if d > rpixel:
                 star[i,j] = 0.
+            elif (d/rpixel)**2 <= 1.:
+                mu = np.sqrt(1. - (d/rpixel)**2)
+                star[i,j] = 1. - u[0]*(1-mu) - u[1]*((1-mu)**2)
 
     # Set out-of-transit flux:
     oot_flux = np.sum(star)
@@ -79,9 +100,8 @@ def numerical_batman(X,Y,rp,npixels = 1000):
     # Iterate through X-coordinates
     for i in range(len(X)):
         # Rescale coordinates to pixel-space:
-        cX, cY = (X+1)*rpixel, (Y+1)*rpixel
+        cX, cY = (X[i]+1)*rpixel, (Y[i]+1)*rpixel
         d = np.sqrt(X[i]**2 + Y[i]**2)
         if d >= 1+rp:
             fluxes = np.append(fluxes, oot_flux)
         else:
-            
